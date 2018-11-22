caef3b7c5365bd401c800b0d1f1fe767c85c3ae2
json2pen <json2pen@gmail.com>
2018-11-22 17:08:03 +0800

    <A>新增android沉浸式状态栏工具类

--- BaseWebview/src/main/java/com/json/basewebview/Utils/SteepStatusBarUtils.java
+++ BaseWebview/src/main/java/com/json/basewebview/Utils/SteepStatusBarUtils.java
@@ -2,6 +2,7 @@ package com.json.basewebview.Utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
 import android.view.View;
 import android.view.Window;
@@ -13,7 +14,7 @@ import android.view.WindowManager;
 
 public class SteepStatusBarUtils {
     /**
-     * 代码这是状态栏透明,并使头部距顶部一个状态栏的高度
+     * 代码这是状态栏透明,并使头部padding顶部一个状态栏的高度
      * @param view 头布局
      * @param mAct 当前activity
      */
@@ -33,5 +34,24 @@ public class SteepStatusBarUtils {
             result = context.getResources().getDimensionPixelSize(resourceId);
         }
         return result;
+    }
+
+    /**
+     * 直接将布局移至statusbar的位置
+     * @param mAct
+     */
+    public static void setSteep(Activity mAct){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //5.0及以上
+            View decorView = mAct.getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            mAct.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //4.4到5.0
+            WindowManager.LayoutParams localLayoutParams = mAct.getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
     }
 }
